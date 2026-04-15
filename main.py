0f62134029d48e0e2a07d01df27fd309a161220f
Dmitrii Goldyrev <dharani@mail.ru>
2026-04-15 12:04:49 +0800

    add check fncs in widget.py in main.py and check both by lints

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+# Импорт функция из модулей masks.py и widget.py
 from src.masks import get_mask_account, get_mask_card_number
+from src.widget import get_date, mask_account_card
 
 
-def main() -> None:
+# Проверка правильной работы функция из модуля masks
+def checking_masks_functions() -> None:
+    """Тест на правильность маскировки номера карты и счета"""
+
     card_number = "7000792289606361"  # пример номера карты
     account_number = "73654108430135874305"  # пример номера счета
 
@@ -12,5 +17,37 @@ def main() -> None:
     print(f"Замаскированный номер счета: {masked_account}")
 
 
+# Проверка правильности функций mask_account_card из модуля widget
+def checking_widget_functions1(account_card: str) -> str:
+    """Тест правильности маскировки номера карты и счета"""
+
+    hidden_account_card = mask_account_card(account_card)
+
+    return hidden_account_card
+
+
+# Проверка правильности работы функции get_date из модуля widget
+def checking_widget_functions2(date_iso_8601: str) -> str:
+    """Тест правильности извлечения даты в обычном формате из международного стандарта написания даты и времени"""
+
+    extracted_date = get_date(date_iso_8601)
+
+    return extracted_date
+
+
 if __name__ == "__main__":
-    main()
+    checking_masks_functions()
+    print(checking_widget_functions1("Счет 64686473678894779589"))
+    print(checking_widget_functions2("2024-03-11T02:26:18.671407"))
+
+    """
+    Примеры входных данных для проверки функции модуля widget.py
+    Maestro 1596837868705199
+    Счет 64686473678894779589
+    MasterCard 7158300734726758
+    Счет 35383033474447895560
+    Visa Classic 6831982476737658
+    Visa Platinum 8990922113665229
+    Visa Gold 5999414228426353
+    Счет 73654108430135874305
+    """
--- src/widget.py
+++ src/widget.py
@@ -1,16 +1,19 @@
-from datetime import datetime
+# Импорт встроенных модулей Python - re и datetime
 import re
+from datetime import datetime
 
-from masks import get_mask_card_number, get_mask_account
+# Импорт функций проекта из masks
+from src.masks import get_mask_account, get_mask_card_number
+
 
 def mask_account_card(account_card: str) -> str:
     """Маскирует полученный номер карты или счета"""
 
-    if "Счет" in account_card:
+    if "Счет" in account_card:  # Создается маска для счета
         letters_part = "".join(re.findall(r"\D+", account_card))
         numbers_part = "".join(re.findall(r"\d+", account_card))
         hidden_account_card = f"{letters_part} {get_mask_account(numbers_part)}"
-    else:
+    else:  # Создается маска для номера карты
         letters_part = "".join(re.findall(r"\D+", account_card))
         numbers_part = "".join(re.findall(r"\d+", account_card))
         hidden_account_card = f"{letters_part} {get_mask_card_number(numbers_part)}"
@@ -19,14 +22,8 @@ def mask_account_card(account_card: str) -> str:
 
 
 def get_date(date_iso_8601: str) -> str:
-    """Конвертирует дату в необходимый формат 'ДД.ММ.ГГГГ'"""
+    """Конвертирует дату из международного стандарта в обычный формат 'ДД.ММ.ГГГГ'"""
 
-    #formatted_date = 'f"{unformatted_date[8:10]}.{unformatted_date[5:7]}.{unformatted_date[:4]}"'
     formatted_date = datetime.strptime(date_iso_8601, "%Y-%m-%dT%H:%M:%S.%f")
+
     return formatted_date.strftime("%d.%m.%Y")
-
-
-if __name__ == '__main__':
-    print(mask_account_card("Visa Platinum 7000792289606361"))  # Visa Platinum 7000 79** **** 6361
-
-    print(get_date("2024-03-11T02:26:18.671407"))  # "11.03.2024"
