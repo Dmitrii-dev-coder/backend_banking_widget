f7d6c9a4ae5880d05666a4493fe99475d91d9cb2
Dmitrii Goldyrev <dharani@mail.ru>
2026-06-03 18:57:53 +0800

    Added to main.py examples of using functions from generators.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # Импорт функция из модулей masks.py и widget.py
+from conftest import valid_data
 from src.masks import get_mask_account, get_mask_card_number
 from src.processing import filter_by_state, sort_by_date
 from src.widget import get_date, mask_account_card
 from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
+from typing import Dict, Generator, Iterator, List
 
 
 # Проверка правильной работы функций из модуля masks
@@ -52,6 +54,98 @@ def checking_sort_by_date(my_list: list, descending: bool = True) -> list:
     return sorted_list
 
 
+# Примеры использования функций модуля generators.py:
+# 0. filter_by_currency.
+
+transaction = [
+        {
+            "id": 939719570,
+            "state": "EXECUTED",
+            "date": "2018-06-30T02:08:58.425572",
+            "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+            "description": "Перевод организации",
+            "from": "Счет 75106830613657916952",
+            "to": "Счет 11776614605963066702",
+        },
+        {
+            "id": 142264268,
+            "state": "EXECUTED",
+            "date": "2019-04-04T23:20:05.206878",
+            "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
+            "description": "Перевод со счета на счет",
+            "from": "Счет 19708645243227258542",
+            "to": "Счет 75651667383060284188",
+        },
+        {
+            "id": 873106923,
+            "state": "EXECUTED",
+            "date": "2019-03-23T01:09:46.296404",
+            "operationAmount": {"amount": "43318.34", "currency": {"name": "руб.", "code": "RUB"}},
+            "description": "Перевод со счета на счет",
+            "from": "Счет 44812258784861134719",
+            "to": "Счет 74489636417521191160",
+        },
+        {
+            "id": 895315941,
+            "state": "EXECUTED",
+            "date": "2018-08-19T04:27:37.904916",
+            "operationAmount": {"amount": "56883.54", "currency": {"name": "USD", "code": "USD"}},
+            "description": "Перевод с карты на карту",
+            "from": "Visa Classic 6831982476737658",
+            "to": "Visa Platinum 8990922113665229",
+        },
+        {
+            "id": 594226727,
+            "state": "CANCELED",
+            "date": "2018-09-12T21:27:25.241689",
+            "operationAmount": {"amount": "67314.70", "currency": {"name": "руб.", "code": "RUB"}},
+            "description": "Перевод организации",
+            "from": "Visa Platinum 1246377376343588",
+            "to": "Счет 14211924144426031657",
+        },
+    ]
+def checking_filter_by_currency(transaction_list: List[Dict], currency: str) -> Iterator[Dict]:
+    """
+    Пример использования функции filter_by_currency.
+    Принимает:
+        transaction (List[Dict]): Список словарей с транзакциями.
+    Возвращает:
+        при обращении (через next()) возвращает отфильтрованные по коду валюты транзакции.
+    """
+    usd_transactions = filter_by_currency(transaction_list, currency)
+
+    return usd_transactions
+
+
+# 1. transaction_descriptions.
+def checking_transaction_descriptions(transactions: List[Dict]) -> Generator[str, None, None]:
+    """
+    Пример использования функции transaction_descriptions.
+    Принимает:
+        transaction (List[Dict]): Список словарей с транзакциями.
+    Возвращает:
+        через next() - описание транзакции str ("description").
+    """
+    descriptions = transaction_descriptions(transactions)
+
+    return descriptions
+
+
+# 2. card_number_generator.
+def checking_card_number_generator(start: int, stop: int) -> Generator[str, None, None]:
+    """
+    Пример использования функции transaction_descriptions.
+    Принимает:
+        start - начальное значение диапазона;
+        stop - конечное значение диапазона.
+    Возвращает:
+        номера банковских карт в формате XXXX XXXX XXXX XXXX.
+    """
+    number_generator = card_number_generator(start, stop)
+
+    return number_generator
+
+
 # Запуск функций из модулей
 if __name__ == "__main__":
     checking_masks_functions()
@@ -96,5 +190,77 @@ if __name__ == "__main__":
         )
     )
 
+# Примеры использования функций модуля generators.py:
+# 0. filter_by_currency.
 
+    result = checking_filter_by_currency(transaction, "USD")
+    try:
+        for _ in range(2):
+            print(next(result))
+    except StopIteration:
+        print("Итератор исчерпан.")
 
+    # Сверка результат:
+    '''
+     {
+        "id": 939719570,
+        "state": "EXECUTED",
+        "date": "2018-06-30T02:08:58.425572",
+        "operationAmount": {
+            "amount": "9824.07",
+            "currency": {
+                "name": "USD",
+                "code": "USD"
+            }
+        },
+        "description": "Перевод организации",
+        "from": "Счет 75106830613657916952",
+        "to": "Счет 11776614605963066702"
+    }
+    {
+        "id": 142264268,
+        "state": "EXECUTED",
+        "date": "2019-04-04T23:20:05.206878",
+        "operationAmount": {
+            "amount": "79114.93",
+            "currency": {
+                "name": "USD",
+                "code": "USD"
+            }
+        },
+        "description": "Перевод со счета на счет",
+        "from": "Счет 19708645243227258542",
+        "to": "Счет 75651667383060284188"
+    }
+    '''
+
+# 1. transaction_descriptions.
+result = checking_transaction_descriptions(transaction)
+try:
+    for _ in range(5):
+        print(next(result))
+except StopIteration:
+    print("Итератор исчерпан.")
+
+# Сверка результат:
+'''
+"Перевод организации",
+"Перевод со счета на счет",
+"Перевод со счета на счет",
+"Перевод с карты на карту",
+"Перевод организации"
+'''
+
+# 2. card_number_generator.
+
+for card_number in checking_card_number_generator(1, 5):
+    print(card_number)
+
+# Сверка результат:
+'''
+0000 0000 0000 0001
+0000 0000 0000 0002
+0000 0000 0000 0003
+0000 0000 0000 0004
+0000 0000 0000 0005
+'''
