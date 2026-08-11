590e8381dc096652dbd4210941ad048c8e26f20a
Dmitrii Goldyrev <dharani@mail.ru>
2026-08-11 10:08:50 +0800

    Add added FileNotFoundError handling in the func - get_transactions_from_file

--- src/utils.py
+++ src/utils.py
@@ -7,14 +7,19 @@ def get_transactions_from_file(file_path):
     Возвращает: список словарей с данными о финансовых транзакциях.
     Если файл пустой, содержит не список или не найден, функция возвращает пустой список.
     """
-
-    with open(file_path, 'r', encoding='utf-8') as operations_file:
-        try:
-            transactions = json.loads('operations_file')
-        except json.JSONDecodeError:
-            transactions = []
+    try:
+        with open(file_path, 'r', encoding='utf-8') as operations_file:
+            try:
+                transactions = json.loads(operations_file.read())
+                if not isinstance(transactions, list):
+                    return []
+            except json.JSONDecodeError:
+                transactions = []
+    except FileNotFoundError:
+        return []
 
     return transactions
+
 
 if __name__ == "__main__":
     result = get_transactions_from_file('data/operations.json')
